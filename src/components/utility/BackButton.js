51f88cd0db4c21937a044739136c3c06ad90544e
rgowan <ranegowan@gmail.com>
2017-09-28 16:41:12 +0100

    renaming index.js

--- src/components/utility/BackButton.js
+++ src/components/utility/BackButton.js
@@ -3,10 +3,7 @@ import React from 'react';
 const BackButton = ({history}) => {
   return (
     <div>
-      <button
-        onClick={() => history.goBack()}
-        className="main-button"
-      >
+      <button onClick={() => history.goBack()} className="main-button">
         <i className="fa fa-arrow-left" aria-hidden="true"></i>Back
       </button>
     </div>
